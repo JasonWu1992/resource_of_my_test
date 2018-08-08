ebd7f45494769bfa9e2ee9a7ef315c3f1242268a
unknown <1054474792@qq.com>
2018-08-08 22:32:56 +0800

    update tornado starting

---
+++ Tornado_studying/test_1/project-one/server.py
@@ -0,0 +1,25 @@
+import tornado.web
+import tornado.ioloop
+import os
+
+abs_path = os.path.dirname(os.path.abspath("__file__"))
+print(abs_path)
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        para = self.get_argument('test', None)
+        self.write("test tornado {}".format(para))
+
+
+if __name__ == '__main__':
+    app = tornado.web.Application(
+        # 这里是路由名称
+        [
+            (r'/GET', IndexHandler)
+        ],
+        static_path=os.path.join(abs_path, 'static')
+    )
+    # 监听端口
+    app.listen(8000)
+    tornado.ioloop.IOLoop.current().start()
